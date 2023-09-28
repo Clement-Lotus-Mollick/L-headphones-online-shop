83c65d95b00fa5c4c6991894cfacbcd18c46c558
Clement-Lotus-Mollick <augustempire999@gmail.com>
2023-09-28 15:29:03 +0600

    anim lotus landing page completed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+  }
+
+//   lotus code C L M H landing page done 
